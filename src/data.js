7fcb9006699f250bc9384cada67603c75f634475
Arman <Shaqaryan99@bk.ru>
2020-07-03 01:36:02 +0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Iphone XS",
+        img:"./img/IphoneXS.png",
+        price: 999,
+        company: "Apple",
+        info: " is simply dummy text of the printing and typesetting industry.",
+        inCart:true,
+        count:0,
+        total:0
+        },
+    {
+        id: 2,
+        title: "Iphone 11 Pro",
+        img:"./img/Iphone11Pro.jpg",
+        price: 979,
+        company: "Iphone11Pro",
+        info: " is simply dummy text of the printing and typesetting industry.",
+        inCart:false,
+        count:0,
+        total:0
+        },
+    {
+        id: 3,
+        title: "Samsung S20",
+        img:"./img/SamsungS20.jpg",
+        price: 997,
+        company: "Samsung",
+        info: " is simply dummy text of the printing and typesetting industry.",
+        inCart:false,
+        count:0,
+        total:0
+        },
+    {
+        id: 4,
+        title: "Xiaomi Redmi Node 8",
+        img:"./img/Xiaomi Note.jpg",
+        price: 700,
+        company: "Xiaomi",
+        info: " is simply dummy text of the printing and typesetting industry.",
+        inCart:false,
+        count:0,
+        total:0
+        }
+    ]
+
+    export const detailProduct =  {
+            id: 1,
+            title: "Google Pixel- Black",
+            img:"./img/IphoneXS.png",
+            price: 999,
+            company: "Apple",
+            info: " is simply dummy text of the printing and typesetting industry.",
+            inCart:false,
+            count:0,
+            total:0
+            };
